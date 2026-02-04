6beeb3f0a56c9f9faa01080aea7f086bf345fb8e
dareymorales55-ux <dareymorales55@gmail.com>
2026-02-03 21:46:56 -0800

    Update ProjectAnonymous.java

--- src/main/java/com/deejay/projectanonymous/ProjectAnonymous.java
+++ src/main/java/com/deejay/projectanonymous/ProjectAnonymous.java
@@ -3,6 +3,7 @@ package com.deejay.projectanonymous;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.deejay.projectanonymous.commands.RevealCommand;
 import com.deejay.projectanonymous.listeners.DeathListener;
 import com.deejay.projectanonymous.listeners.JoinListener;
 import com.deejay.projectanonymous.reveal.HourlyReveal;
@@ -14,7 +15,7 @@ public class ProjectAnonymous extends JavaPlugin {
     @Override
     public void onEnable() {
 
-        // Ensure NickAPI is present
+        // Ensure NickAPI is present (skins only)
         if (Bukkit.getPluginManager().getPlugin("NickAPI") == null) {
             getLogger().severe("NickAPI not found! Disabling ProjectAnonymous.");
             Bukkit.getPluginManager().disablePlugin(this);
@@ -37,6 +38,11 @@ public class ProjectAnonymous extends JavaPlugin {
                 this
         );
 
+        // Register commands
+        getCommand("reveal").setExecutor(
+                new RevealCommand(this)
+        );
+
         getLogger().info("ProjectAnonymous enabled.");
     }
 
@@ -46,7 +52,7 @@ public class ProjectAnonymous extends JavaPlugin {
     }
 
     /**
-     * Utility method (optional)
+     * Utility method (optional, not required by logic)
      */
     public boolean isNicked(org.bukkit.entity.Player player) {
         return NickAPI.isNicked(player);
