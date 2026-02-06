646aa311062f94c66db756f7b779b747beb6a0b2
dareymorales55-ux <dareymorales55@gmail.com>
2026-02-05 20:39:53 -0800

    Update GiveBellCommand.java

--- src/main/java/com/deejay/projectanonymous/commands/GiveBellCommand.java
+++ src/main/java/com/deejay/projectanonymous/commands/GiveBellCommand.java
@@ -1,7 +1,6 @@
 package com.deejay.projectanonymous.commands;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,31 +12,29 @@ import com.deejay.projectanonymous.items.BellOfTruth;
 public class GiveBellCommand implements CommandExecutor {
 
     private final ProjectAnonymous plugin;
+    private final BellOfTruth bell;
 
     public GiveBellCommand(ProjectAnonymous plugin) {
         this.plugin = plugin;
+        this.bell = new BellOfTruth(plugin);
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if (args.length != 1) {
-            sender.sendMessage(ChatColor.RED + "Usage: /givebell <player>");
+            sender.sendMessage("§cUsage: /givebell <player>");
             return true;
         }
 
-        Player target = Bukkit.getPlayerExact(args[0]);
+        Player target = Bukkit.getPlayer(args[0]);
         if (target == null) {
-            sender.sendMessage(ChatColor.RED + "Player not found.");
+            sender.sendMessage("§cPlayer not found.");
             return true;
         }
 
-        BellOfTruth bell = new BellOfTruth(plugin);
         target.getInventory().addItem(bell.createBell());
-
-        sender.sendMessage(ChatColor.GREEN + "Gave Bell of Truth to " + target.getName());
-        target.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "You received the Bell of Truth");
-
+        sender.sendMessage("§aGave Bell of Truth to §e" + target.getName());
         return true;
     }
 }
